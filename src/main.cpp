65fe0a8d4e84622c799932ba41a9292580bf0cfc
Felix Morgner <felix.morgner@gmail.com>
2017-08-26 19:01:13 +0200

    demo: switch to registers demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
-#include "avr/sleep.hpp"
+#include "avr/internal/registers.hpp"
+
+struct some_device
+  {
+  struct gpio_ports
+    {
+    static auto constexpr B = avr::internal::registers::io_register_8<0x03>;
+    };
+  };
 
 int main()
   {
-  avr::sleep::select_enter<avr::sleep::mode::idle>();
+  auto constexpr & portB = some_device::gpio_ports::B;
+
+  if(portB.bit<3>.get())
+    {
+    portB.bit<7>.set();
+    }
+  else
+    {
+    portB.bit<7>.clear();
+    }
   }
