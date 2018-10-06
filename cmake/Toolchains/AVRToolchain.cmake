8bb1e13cd9ed82e3fb6d273450b7e255797bb214
Felix Morgner <felix.morgner@gmail.com>
2018-10-06 10:48:58 +0200

    build: move add_firmware to toolchain

--- cmake/Toolchains/AVRToolchain.cmake
+++ cmake/Toolchains/AVRToolchain.cmake
@@ -16,3 +16,30 @@ if(NOT AVR_GCC)
   set(CMAKE_SYSTEM_NAME "Generic")
   set(CMAKE_SYSTEM_PROCESSOR "avr")
 endif()
+
+function(add_firmware TARGET)
+  set(OPTIONS HEX)
+  set(SINGLE_VALUE_ARGUMENTS MCU CLOCK_FREQUENCY)
+  set(MULTI_VALUE_ARGUMENTS SOURCES)
+  cmake_parse_arguments(ADD_FIRMWARE
+    "${OPTIONS}"
+    "${SINGLE_VALUE_ARGUMENTS}"
+    "${MULTI_VALUE_ARGUMENTS}"
+    ${ARGN}
+    )
+
+  add_executable(${TARGET} ${ADD_FIRMWARE_SOURCES})
+  set_target_properties(${TARGET} PROPERTIES
+    COMPILE_FLAGS "-mmcu=${ADD_FIRMWARE_MCU}"
+    LINK_FLAGS "-mmcu=${ADD_FIRMWARE_MCU}"
+    COMPILE_DEFINITIONS "F_CPU=${ADD_FIRMWARE_CLOCK_FREQUENCY}"
+    )
+  get_target_property(${TARGET}_HEX ${TARGET} RUNTIME_OUTPUT_DIRECTORY)
+  add_custom_command(TARGET ${TARGET}
+    POST_BUILD
+    COMMAND ${CMAKE_OBJCOPY} -j .text -j .data -O ihex $<TARGET_FILE:${TARGET}> $<TARGET_FILE:${TARGET}>.hex
+    )
+  add_custom_target(${TARGET}_disassemble
+    COMMAND ${CMAKE_OBJDUMP} -d $<TARGET_FILE:${TARGET}>
+    )
+endfunction()
